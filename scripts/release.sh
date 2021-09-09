c4d6abf13e62e3b8950daec9787eb22f680fd240
epwalsh <epwalsh10@gmail.com>
2021-09-09 10:15:51 -0700

    add prompt

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,15 @@
 set -e
 
 TAG=$(python -c 'from cached_path.version import VERSION; print("v" + VERSION)')
-git commit -a -m "Bump version to $TAG for release" || true && git push
-echo "Creating new git tag $TAG"
-git tag "$TAG" -m "$TAG"
-git push --tags
+
+read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+    git commit -a -m "Bump version to $TAG for release" || true && git push
+    echo "Creating new git tag $TAG"
+    git tag "$TAG" -m "$TAG"
+    git push --tags
+else
+    echo "Cancelled"
+    exit 1
+fi
