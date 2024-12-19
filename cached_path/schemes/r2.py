b45bec31ce050386ffcfb51bf3a47ce40fe223cc
Shane A <shanea@allenai.org>
2024-12-19 10:33:29 -0800

    Cache R2 and S3 clients

--- cached_path/schemes/r2.py
+++ cached_path/schemes/r2.py
@@ -4,6 +4,7 @@ Cloudflare R2.
 
 import io
 import os
+from functools import cache
 from typing import Optional
 
 import boto3.dynamodb
@@ -27,36 +28,7 @@ class R2Client(SchemeClient):
         SchemeClient.__init__(self, resource)
         self.bucket_name, self.path = _split_cloud_path(resource, "r2")
 
-        # find credentials
-        endpoint_url = os.environ.get("R2_ENDPOINT_URL")
-        if endpoint_url is None:
-            raise ValueError(
-                "R2 endpoint url is not set. Did you forget to set the 'R2_ENDPOINT_URL' env var?"
-            )
-        profile_name = os.environ.get("R2_PROFILE")
-        access_key_id = os.environ.get("R2_ACCESS_KEY_ID")
-        secret_access_key = os.environ.get("R2_SECRET_ACCESS_KEY")
-        if access_key_id is not None and secret_access_key is not None:
-            session_kwargs = {
-                "aws_access_key_id": access_key_id,
-                "aws_secret_access_key": secret_access_key,
-            }
-        elif profile_name is not None:
-            session_kwargs = {"profile_name": profile_name}
-        else:
-            raise ValueError(
-                "To authenticate for R2, you either have to set the 'R2_PROFILE' env var and set up this profile, "
-                "or set R2_ACCESS_KEY_ID and R2_SECRET_ACCESS_KEY."
-            )
-
-        s3_session = boto3.session.Session(**session_kwargs)
-
-        self.s3 = s3_session.client(
-            service_name="s3",
-            endpoint_url=endpoint_url,
-            region_name="auto",
-            config=Config(retries={"max_attempts": 10, "mode": "standard"}),
-        )
+        self.s3 = _get_s3_resource_client()
         self.object_info = None
 
     def _ensure_object_info(self):
@@ -88,3 +60,36 @@ class R2Client(SchemeClient):
             Bucket=self.bucket_name, Key=self.path, Range=f"bytes={index}-{index+length-1}"
         )
         return response["Body"].read()
+
+
+@cache
+def _get_s3_resource_client():
+    # find credentials
+    endpoint_url = os.environ.get("R2_ENDPOINT_URL")
+    if endpoint_url is None:
+        raise ValueError(
+            "R2 endpoint url is not set. Did you forget to set the 'R2_ENDPOINT_URL' env var?"
+        )
+    profile_name = os.environ.get("R2_PROFILE")
+    access_key_id = os.environ.get("R2_ACCESS_KEY_ID")
+    secret_access_key = os.environ.get("R2_SECRET_ACCESS_KEY")
+    if access_key_id is not None and secret_access_key is not None:
+        session_kwargs = {
+            "aws_access_key_id": access_key_id,
+            "aws_secret_access_key": secret_access_key,
+        }
+    elif profile_name is not None:
+        session_kwargs = {"profile_name": profile_name}
+    else:
+        raise ValueError(
+            "To authenticate for R2, you either have to set the 'R2_PROFILE' env var and set up this profile, "
+            "or set R2_ACCESS_KEY_ID and R2_SECRET_ACCESS_KEY."
+        )
+
+    s3_session = boto3.session.Session(**session_kwargs)
+    return s3_session.client(
+        service_name="s3",
+        endpoint_url=endpoint_url,
+        region_name="auto",
+        config=Config(retries={"max_attempts": 10, "mode": "standard"}),
+    )
