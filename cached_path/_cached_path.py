0a488ec8eca1a20acea5e08a2c91052fff067363
Kenshi Takayama <kenshi84@gmail.com>
2025-03-18 16:34:36 +0900

    add support for non-tar .bz2 files

--- cached_path/_cached_path.py
+++ cached_path/_cached_path.py
@@ -1,3 +1,4 @@
+import bz2
 import logging
 import os
 import shutil
@@ -32,6 +33,10 @@ if TYPE_CHECKING:
 logger = logging.getLogger("cached_path")
 
 
+def _is_bz2file(path: PathOrStr) -> bool:
+    return Path(path).suffix.lower() == ".bz2"
+
+
 def _is_rarfile(path: PathOrStr, url_or_filename: PathOrStr) -> bool:
     try:
         import rarfile
@@ -53,6 +58,7 @@ def _is_archive(file_path: PathOrStr, url_or_filename) -> bool:
     return (
         is_zipfile(file_path)
         or tarfile.is_tarfile(file_path)
+        or _is_bz2file(url_or_filename)
         or _is_rarfile(file_path, url_or_filename)
     )
 
@@ -241,9 +247,20 @@ def cached_path(
             raise ValueError(f"unable to parse {orig_url_or_filename} as a URL or as a local path")
 
     if extraction_path is not None:
+        def _get_bz2_extract_symlink():
+            assert _is_bz2file(url_or_filename)
+            symlink_dir = file_path.parent / (file_path.name + "-symlink")
+            symlink_dir.mkdir(exist_ok=True)
+            meta = Meta.from_path(str(file_path) + ".json")
+            symlink_path = symlink_dir / Path(meta.resource).name[:-4]
+            if not symlink_path.exists():
+                symlink_path.symlink_to(extraction_path/Path(meta.resource).name[:-4])
+            return symlink_path
         # If the extracted directory already exists (and is non-empty), then no
         # need to create a lock file and extract again unless `force_extract=True`.
         if os.path.isdir(extraction_path) and os.listdir(extraction_path) and not force_extract:
+            if symlink:
+                return _get_bz2_extract_symlink()
             return extraction_path
 
         # Extract it.
@@ -276,6 +293,10 @@ def cached_path(
 
                     with rarfile.RarFile(file_path) as rar_file:
                         rar_file.extractall(tmp_extraction_dir)
+                elif _is_bz2file(url_or_filename):
+                    with bz2.BZ2File(file_path) as bz2_file:
+                        with open(f'{tmp_extraction_dir}/{Path(url_or_filename).name[:-4]}', "wb") as out_file:
+                            shutil.copyfileobj(bz2_file, out_file)
                 else:
                     with ZipFile(file_path) as zip_file:
                         zip_file.extractall(tmp_extraction_dir)
@@ -292,6 +313,8 @@ def cached_path(
             finally:
                 shutil.rmtree(tmp_extraction_dir, ignore_errors=True)
 
+        if symlink:
+            return _get_bz2_extract_symlink()
         return extraction_path
 
     if symlink:
